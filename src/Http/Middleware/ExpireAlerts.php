90f432cfc35619dbfff5f57748c0dd1749c69864
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2019-11-17 01:08:27 -0300

    Removed unnecessary session check for the Alert Bag.

--- src/Http/Middleware/ExpireAlerts.php
+++ src/Http/Middleware/ExpireAlerts.php
@@ -4,7 +4,6 @@ namespace DarkGhostHunter\Laralerts\Http\Middleware;
 
 use Closure;
 use DarkGhostHunter\Laralerts\AlertBag;
-use Illuminate\Contracts\Session\Session;
 
 class ExpireAlerts
 {
@@ -16,22 +15,13 @@ class ExpireAlerts
     protected $alertBag;
 
     /**
-     * The Session handler
-     *
-     * @var \Illuminate\Contracts\Session\Session
-     */
-    protected $session;
-
-    /**
      * Create a new ExpireAlerts instance.
      *
      * @param  \DarkGhostHunter\Laralerts\AlertBag $alertBag
-     * @param  \Illuminate\Contracts\Session\Session $session
      */
-    public function __construct(AlertBag $alertBag, Session $session)
+    public function __construct(AlertBag $alertBag)
     {
         $this->alertBag = $alertBag;
-        $this->session = $session;
     }
 
     /**
@@ -58,8 +48,6 @@ class ExpireAlerts
         // After the response is sent to the browser, we will age the alerts automatically
         // since they should be already rendered. With this we can know if in the next
         // Request these old alerts should be recovered to show them again or not.
-        if ($this->session->isStarted()) {
-            $this->alertBag->ageAlerts();
-        }
+        $this->alertBag->ageAlerts();
     }
 }
