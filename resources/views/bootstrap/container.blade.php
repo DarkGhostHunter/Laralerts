e2be4d92561f11bdb46305cdc64435880dafb245
Italo <DarkGhostHunter@Gmail.com>
2021-11-17 14:19:54 -0300

    Unscapes attributes.

--- resources/views/bootstrap/container.blade.php
+++ resources/views/bootstrap/container.blade.php
@@ -1,5 +1,5 @@
 @if($alerts->isNotEmpty())
-    <div {{ $attributes ?? 'class="alerts"' }}>
+    <div {!! $attributes !!? 'class="alerts"' }}>
         @each('laralerts::bootstrap.alert', $alerts, 'alert')
     </div>
 @endif
